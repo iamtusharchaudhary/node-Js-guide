011081d54ee909645c7e0a157da1afe160d0c06c
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-05-03 21:28:11 +0530

    Update index.js

--- Introduction/index.js
+++ Introduction/index.js
@@ -1,1 +1,13 @@
+const { create } = require('domain');
+const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 4000;
+
+const server = http.createServer((req,res)=>{
+  res.writeHead(200,{'content-type':'text/plain'});
+  res.end("Tushar Chaudhary Developer");
+})
+server.listen(port,hostname,()=>{
+  console.log(`server running at http://${hostname}:${port}`)
+})
