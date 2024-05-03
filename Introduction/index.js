e085da4cab6d6115a2695c3de1da0ca80da7afe4
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-05-03 21:29:38 +0530

    Update index.js

--- Introduction/index.js
+++ Introduction/index.js
@@ -1,8 +1,13 @@
 const { create } = require('domain');
+
+// create http server
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 4000;
+
+// create own server
 
 const server = http.createServer((req,res)=>{
   res.writeHead(200,{'content-type':'text/plain'});
