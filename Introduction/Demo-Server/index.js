58219ccc840d97ad225328697ecc2d7dfbc073d9
Tushar Chaudhary <149557229+iamtusharchaudhary@users.noreply.github.com>
2024-07-18 10:13:11 +0530

    Update index.js

--- Introduction/Demo-Server/index.js
+++ Introduction/Demo-Server/index.js
@@ -1,4 +1,27 @@
+// This is module type:-
+
 import express from 'express';
+const app = express();
+
+const port = 5000;
+
+app.get('/',(req,res)=>{
+    res.send('Tushar Chaudhary');
+})
+app.get('/tushar',(req,res)=>{
+    res.send('<h1>Tushar Chaudhary Developer</h1>');
+})
+app.get('/login',(req,res)=>{
+    res.send('<h4>User Login Successfully</h4>');
+})
+app.listen(port,()=>{
+    console.log(`server running http://localhost${port}`);
+})
+
+// This is commonjs type:-
+
+
+const express = require('express');
 const app = express();
 
 const port = 5000;
